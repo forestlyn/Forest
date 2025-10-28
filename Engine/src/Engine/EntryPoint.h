fd9d9a3284ad02ea85396e95ef17be17ff7544ad
forestlyn <yang2797817213@qq.com>
2025-10-28 22:17:48 +0800

    实现cmake编译，删除vs生成的sln等

--- Engine/src/Engine/EntryPoint.h
+++ Engine/src/Engine/EntryPoint.h
@@ -1,11 +1,12 @@
 #pragma once
 #ifdef FOREST_PLATFORM_WINDOWS
-extern Engine::Application* Engine::CreateApplication();
+extern Engine::Application *Engine::CreateApplication();
 
-int main(int argc,char **argv) {
+int main(int argc, char **argv)
+{
 	Engine::Log::Init();
-
-	Engine::Application* app = Engine::CreateApplication();
+	ENGINE_INFO("hjaha	");
+	Engine::Application *app = Engine::CreateApplication();
 	app->Run();
 	delete app;
 	return 0;
