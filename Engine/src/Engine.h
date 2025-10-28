12fe404a3f53f6253d1fac384be940c272eb3c86
forestlyn <yang2797817213@qq.com>
2025-10-28 16:35:44 +0800

    实现Log

--- Engine/src/Engine.h
+++ Engine/src/Engine.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Engine/Application.h"
+#include "Engine/Log.h"
 
 //--------entry point-------------
 #include"Engine/EntryPoint.h"
--- Engine/src/Engine/EntryPoint.h
+++ Engine/src/Engine/EntryPoint.h
@@ -2,7 +2,9 @@
 #ifdef FOREST_PLATFORM_WINDOWS
 extern Engine::Application* Engine::CreateApplication();
 
-int main(int args,char **argv) {
+int main(int argc,char **argv) {
+	Engine::Log::Init();
+
 	Engine::Application* app = Engine::CreateApplication();
 	app->Run();
 	delete app;
