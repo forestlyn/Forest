122b69c1411169ae55ed0eee4d103c6b59e859ac
forestlyn <yang2797817213@qq.com>
2025-11-03 20:04:57 +0800

    fix bug：链接问题导致 修改为静态库

--- Engine/src/Engine/Core/Core.h
+++ Engine/src/Engine/Core/Core.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #ifdef FOREST_PLATFORM_WINDOWS
+#ifdef FOREST_DYNAMIC_LINK
 #ifdef FOREST_BUILD_DLL
 #define FOREST_API __declspec(dllexport)
 #else
 #define FOREST_API __declspec(dllimport)
+#endif
+#else
+#define FOREST_API
 #endif
 #else
 #error Forest only supports Windows!
--- Engine/src/Engine/ImGui/ImGuiLayer.cpp
+++ Engine/src/Engine/ImGui/ImGuiLayer.cpp
@@ -1,7 +1,6 @@
 #include "ImGuiLayer.h"
 #include "KeyMap.h"
 #include "Engine/Core/Application.h"
-#include "imgui.h"
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 #include <GLFW/glfw3.h>
