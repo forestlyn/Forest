8c12a190f89a09b9522e00fc15a87c04a1b0267b
forestlyn <yang2797817213@qq.com>
2026-01-13 10:56:53 +0800

    增加下载vulkansdk脚本

---
+++ Scripts/Setup.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+import CheckPython
+
+# Make sure everything we need is installed
+CheckPython.ValidatePackages()
+
+import Vulkan
+
+# Change from Scripts directory to root
+os.chdir("../")
+
+if not Vulkan.CheckVulkanSDK():
+    print("Vulkan SDK not installed.")
---
+++ Scripts/Vulkan.py
@@ -0,0 +1,49 @@
+import os
+import subprocess
+import sys
+from pathlib import Path
+
+import Utils
+
+from io import BytesIO
+from urllib.request import urlopen
+from zipfile import ZipFile
+
+VULKAN_SDK = os.environ.get("VULKAN_SDK")
+VULKAN_SDK_INSTALLER_URL = "https://sdk.lunarg.com/sdk/download/1.4.335.0/windows/vulkansdk-windows-X64-1.4.335.0.exe"
+HAZEL_VULKAN_VERSION = "1.4.335.0"
+VULKAN_SDK_EXE_PATH = "Engine/ThirdParty/VulkanSDK/VulkanSDK.exe"
+
+
+def InstallVulkanSDK():
+    print("Downloading {} to {}".format(VULKAN_SDK_INSTALLER_URL, VULKAN_SDK_EXE_PATH))
+    Utils.DownloadFile(VULKAN_SDK_INSTALLER_URL, VULKAN_SDK_EXE_PATH)
+    print("Done!")
+    print("Running Vulkan SDK installer...")
+    os.startfile(os.path.abspath(VULKAN_SDK_EXE_PATH))
+    print("Re-run this script after installation")
+
+
+def InstallVulkanPrompt():
+    print("Would you like to install the Vulkan SDK?")
+    install = Utils.YesOrNo()
+    if install:
+        InstallVulkanSDK()
+        quit()
+
+
+def CheckVulkanSDK():
+    if VULKAN_SDK is None:
+        print("You don't have the Vulkan SDK installed!")
+        InstallVulkanPrompt()
+        return False
+    elif HAZEL_VULKAN_VERSION not in VULKAN_SDK:
+        print(f"Located Vulkan SDK at {VULKAN_SDK}")
+        print(
+            f"You don't have the correct Vulkan SDK version! (Hazel requires {HAZEL_VULKAN_VERSION})"
+        )
+        InstallVulkanPrompt()
+        return False
+
+    print(f"Correct Vulkan SDK located at {VULKAN_SDK}")
+    return True
