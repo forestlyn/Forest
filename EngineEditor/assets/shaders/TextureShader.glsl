27d7f83ba283de8ec22c9fa955d36cb4cb603c3d
forestlyn <yang2797817213@qq.com>
2026-01-16 13:25:47 +0800

    fix：TexIndex错误插值问题

--- EngineEditor/assets/shaders/TextureShader.glsl
+++ EngineEditor/assets/shaders/TextureShader.glsl
@@ -15,18 +15,18 @@ layout(std140, binding = 0) uniform Camera {
 struct VertexOutput {
 	vec4 Color;
 	vec2 TexCoord;
-	float TexIndex;
 	float TilingFactor;
 };
 
 layout(location = 0) out VertexOutput Output;
+layout(location = 3) out flat float v_TexIndex;
 layout(location = 4) out flat int v_EntityID;
 
 void main() {
 	Output.Color = a_Color;
 	Output.TexCoord = a_TexCoord;
-	Output.TexIndex = a_TexIndex;
 	Output.TilingFactor = a_TilingFactor;
+	v_TexIndex = a_TexIndex;
 	v_EntityID = a_EntityID;
 
 	gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
@@ -41,11 +41,11 @@ layout(location = 1) out int color2;
 struct VertexOutput {
 	vec4 Color;
 	vec2 TexCoord;
-	float TexIndex;
 	float TilingFactor;
 };
 
 layout(location = 0) in VertexOutput Input;
+layout(location = 3) in flat float v_TexIndex;
 layout(location = 4) in flat int v_EntityID;
 
 layout(binding = 0) uniform sampler2D u_Textures[32];
@@ -53,7 +53,7 @@ layout(binding = 0) uniform sampler2D u_Textures[32];
 void main() {
 	vec4 texColor = Input.Color;
 
-	switch(int(Input.TexIndex)) {
+	switch(int(v_TexIndex)) {
 		case 0:
 			texColor *= texture(u_Textures[0], Input.TexCoord * Input.TilingFactor);
 			break;
